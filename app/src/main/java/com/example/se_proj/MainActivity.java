af283713d8d00daebd1a118a9669cb05cbe3834b
maryamibrahim <442201849@student.ksu.edu.sa>
2023-10-18 20:55:56 +0300

    creating database and add post feasure updates

--- app/src/main/java/com/example/se_proj/MainActivity.java
+++ app/src/main/java/com/example/se_proj/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.se_proj;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
+import com.example.se_proj.DatabaseHelper;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri selectedImageUri; // Variable to store the selected image URI
+    private String description = ""; // Initialize the description as an empty string
+    private int userId = 123; // Replace with the actual user ID
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +28,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPostButtonClick(View view) {
-        String imageUrl = "URL_OF_THE_IMAGE"; // Get the URL of the uploaded image
-        String description = "USER_DESCRIPTION"; // Get the user's description
-        int userId = 123; // Replace with the actual user ID
+        // Check if an image has been selected
+        if (selectedImageUri != null) {
+            String imageUrl = "URL_OF_THE_IMAGE"; // Replace with the actual image URL
 
-        // Insert data into the database
-        DatabaseHelper dbHelper = new DatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+            DatabaseHelper dbHelper = new DatabaseHelper(this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(DatabaseHelper.COLUMN_IMAGE_URL, imageUrl);
-        values.put(DatabaseHelper.COLUMN_DESCRIPTION, description);
-        values.put(DatabaseHelper.COLUMN_USER_ID, userId);
-        values.put(DatabaseHelper.COLUMN_TIMESTAMP, System.currentTimeMillis());
+            ContentValues values = new ContentValues();
+            values.put(DatabaseHelper.COLUMN_IMAGE_URL, imageUrl);
+            values.put(DatabaseHelper.COLUMN_DESCRIPTION, description);
+            values.put(DatabaseHelper.COLUMN_USER_ID, userId);
+            values.put(DatabaseHelper.COLUMN_TIMESTAMP, System.currentTimeMillis());
 
-        long newRowId = db.insert(DatabaseHelper.TABLE_POSTS, null, values);
+            long newRowId = db.insert(DatabaseHelper.TABLE_POSTS, null, values);
 
-        db.close();
+            db.close();
 
-        // Provide user feedback, such as a success message
-        Toast.makeText(this, "Post uploaded successfully!", Toast.LENGTH_SHORT).show();
+            // Provide user feedback, such as a success message
+            Toast.makeText(this, "Post uploaded successfully!", Toast.LENGTH_SHORT).show();
+        } else {
+            // Inform the user to select an image before posting
+            Toast.makeText(this, "Please select an image before posting.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onImagePickerButtonClick(View view) {
@@ -53,9 +64,15 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
-            // Handle the selected image, e.g., display it in an ImageView
+            // Store the selected image URI for later use
             Uri selectedImageUri = data.getData();
-            // Perform further processing of the selected image
+
+            // Display the selected image in an ImageView (assuming you have one in your layout)
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageURI(selectedImageUri);
+
+            // Perform further processing of the selected image if needed
         }
     }
 }
+
